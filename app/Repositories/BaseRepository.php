5653687ddf12ba10d788daeec2ed2ff54bda23e7
ferdie <ferdiebergado@gmail.com>
2019-09-04 21:28:06 +0800

    Added BaseRepository

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+use App\BaseModel;
+use App\Repositories\BaseRepositoryInterface;
+
+abstract class BaseRepository implements BaseRepositoryInterface
+{
+    protected $model;
+
+    /**
+     * BaseRepository constructor.
+     * @param Model $model
+     */
+    public function __construct(BaseModel $model)
+    {
+        $this->model = $model;
+    }
+}
